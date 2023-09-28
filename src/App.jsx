09f745f198712494724f52eff626c624ca21c224
Tosinfamzy <famurewatosin@gmail.com>
2023-09-28 21:31:29 +0100

    create layout and home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
+import Layout from './components/Layout';
+import Home from './components/Home';
 
 function App() {
 	return (
-		<main className='App'>
-			<Login />
-			{/* <Register /> */}
-		</main>
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route path='login' element={<Login />} />
+				<Route path='register' element={<Register />} />
+				<Route path='/' element={<Home />} />
+			</Route>
+		</Routes>
 	);
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+	return <div>Home | should only be seen when logged in</div>;
+}
+
+export default Home;
