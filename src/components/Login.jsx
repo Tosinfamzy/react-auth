097fd72be3937d2d12eded27d1bb07046d50dacd
Tosinfamzy <famurewatosin@gmail.com>
2023-09-28 19:26:32 +0100

    complete login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,6 +27,10 @@ function Login() {
 				withCredentials: true,
 			});
 			console.log(response.data);
+			setUser('');
+			setPwd('');
+			setSuccess(true);
+			//TODO:Store tokens in global state (redux or context)
 		} catch (error) {
 			switch (error) {
 				case error.response?.status === 400:
@@ -45,33 +49,37 @@ function Login() {
 
 	return (
 		<>
-			<section>
-				<p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
-					{errMsg}
-				</p>
-				<h1>Login In</h1>
-				<form onSubmit={handleLogin}>
-					<label htmlFor='username'>Username:</label>
-					<input
-						type='text'
-						id='username'
-						ref={userRef}
-						autoComplete='off'
-						onChange={(e) => setUser(e.target.value)}
-						value={user}
-						required
-					/>
-					<label htmlFor='password'>Password:</label>
-					<input
-						type='password'
-						id='password'
-						onChange={(e) => setPwd(e.target.value)}
-						value={pwd}
-						required
-					/>
-					<button>Sign In</button>
-				</form>
-			</section>
+			{success ? (
+				<section></section>
+			) : (
+				<section>
+					<p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'}>
+						{errMsg}
+					</p>
+					<h1>Login In</h1>
+					<form onSubmit={handleLogin}>
+						<label htmlFor='username'>Username:</label>
+						<input
+							type='text'
+							id='username'
+							ref={userRef}
+							autoComplete='off'
+							onChange={(e) => setUser(e.target.value)}
+							value={user}
+							required
+						/>
+						<label htmlFor='password'>Password:</label>
+						<input
+							type='password'
+							id='password'
+							onChange={(e) => setPwd(e.target.value)}
+							value={pwd}
+							required
+						/>
+						<button>Sign In</button>
+					</form>
+				</section>
+			)}
 		</>
 	);
 }
